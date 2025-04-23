d9d1a00cd9715c776501f52a63e8d8504cab5235
ssyu <1525996190@qq.com>
2025-04-23 16:39:50 +0800

    django 初体验

---
+++ app01/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return HttpResponse("Hello, Django!")
+
+
+def user_list(request):
+    return render(request, 'user_list.html')
+
+def user_add(request):
+    return HttpResponse("新增用户")
